bea4fda1e73a5a6d8767d315659d0a3865a733d2
Dentan Aurelien <aurelien.dentan@etu.umontpellier.fr>
2017-10-31 15:34:54 +0100

    Ajout de tab et tableau de donnees

--- main.py
+++ main.py
@@ -6,14 +6,12 @@ Created on Mon Oct 30 10:31:45 2017
 @author: aure
 """
 import sys
-from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtWidgets import QApplication, QComboBox, QFormLayout, QGroupBox, QColorDialog, QWidget,QVBoxLayout,QGridLayout,QLabel,QHBoxLayout,QLineEdit,QPushButton, QFileSystemModel, QTreeView
-from PyQt5.QtGui import QIcon, QPainter, QColor, QPen
-from PyQt5.QtCore import Qt
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem, QTabWidget, QApplication, QComboBox, QFormLayout, QGroupBox, QColorDialog, QWidget, QVBoxLayout, QLabel, QHBoxLayout, QPushButton, QFileSystemModel, QTreeView
 
-import matplotlib
-matplotlib.use('Qt5Agg')
-from matplotlib import animation
+
+
+
 from matplotlib.figure import Figure
 from matplotlib.lines import Line2D
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
@@ -21,10 +19,10 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
 
-class dialog(QtWidgets.QDialog):
+# Fenetre de choix du fichier
+class dialog_fichier(QtWidgets.QDialog):
     
     def __init__(self,Main):
         QtWidgets.QDialog.__init__(self,Main)
@@ -41,10 +39,12 @@ class dialog(QtWidgets.QDialog):
         # Bouton ouvrir
         ouvrir = QtWidgets.QPushButton("ouvrir")
         ouvrir.clicked.connect(self.choix_fichier)
+        ouvrir.setAutoDefault(True)
         
         # Bouton annuler
         annuler = QtWidgets.QPushButton("annuler")
         annuler.clicked.connect(self.reject)
+        annuler.setAutoDefault(False)
         
         layout = QVBoxLayout()
         layout.addWidget(self.tree)
@@ -59,11 +59,96 @@ class dialog(QtWidgets.QDialog):
         self.main.path = self.model.filePath(index) # recupere le path du fichier cliqué
         self.reject() # ferme la fenetre de dialogue
         self.main.parse_file() # lance le parse pour remplir les listes x et y
+
+        
+ # ------------------- Tab 1 (Graphique) -----------------------------       
+class Tab1(QWidget):
+    def __init__(self,parent):
+        super().__init__()
+        # variable pour le data du fichier
+        parent.data =[]
+        #BOuton test
+        parent.test = QPushButton("test")
+        parent.test.clicked.connect(parent.test_func)
+        
+        # Formulaire
+        parent.formGroupBox = QGroupBox("Paramètres")
+        layout = QFormLayout()
+        
+        parent.choose_file = QtWidgets.QPushButton("Choisir le fichier csv")
+        parent.choose_file.clicked.connect(parent.ouvre_arborescence)
+        parent.path = ''
+        
+        
+        parent.colx = QComboBox()
+        parent.coly = QComboBox()
+        
+        parent.tracer = QtWidgets.QPushButton("Tracer")
+        parent.tracer.clicked.connect(parent.Trace)
+        
+        # Choux couleur
+        parent.color_btn = QtWidgets.QPushButton("Choisir couleur")
+        parent.color_btn.clicked.connect(parent.fig_color)
+        parent.color = 'b'
+        
+        # Choix marqueur
+        marqueurs = Line2D.markers.keys()
+        parent.marqueur = '.'
+        parent.mark = QComboBox()
+        for i in marqueurs:
+            parent.mark.addItem(str(i))
+            
+        parent.mark.currentIndexChanged.connect(parent.fig_marqueur)
+        
+        # linestyles
+        parent.current_linestyle = '-'
+        linestyles = ['-', '--', '-.', ':','steps','None']
+        parent.linestyle = QComboBox()
+        for i in linestyles:
+            parent.linestyle.addItem(str(i))
+        parent.linestyle.currentIndexChanged.connect(parent.fig_linestyle)
+        
+        # Tracer
+        parent.fig = Figure()
+        parent.sub1 = parent.fig.add_subplot(111)
+        parent.sub1.plot([],[],'r')
+        
+        
+        
+        
+        parent.canvas = FigureCanvas(parent.fig)
+        parent.tab1.layout.addWidget(parent.canvas)
+        
+        layout.addRow(parent.test)
+        layout.addRow(parent.choose_file)
+        layout.addRow(QtWidgets.QLabel("x: "), parent.colx)
+        layout.addRow(QtWidgets.QLabel("y: "), parent.coly)
+        layout.addRow(parent.tracer)
+        layout.addRow(parent.color_btn)
+        layout.addRow(QtWidgets.QLabel('Marqueur:'), parent.mark)
+        layout.addRow(QtWidgets.QLabel('line:'), parent.linestyle)
+        parent.formGroupBox.setLayout(layout)
+        parent.tab1.layout.addWidget(parent.formGroupBox)
+        
+# ---------------------------------------------------------------------------
+class Tab2(QWidget):
+    
+    def __init__(self, parent):
+        super().__init__()        
+        
+        
+        self.tableau = QTableWidget()
+        
+        
+        self.txt = QtWidgets.QLabel('')
+        parent.tab2.layout.addWidget(self.tableau)
         
         
         
         
         
+        
+# ---------------------------------------------------------------------------       
 class Graphiques(QWidget):
     
     def __init__(self):
@@ -74,74 +159,33 @@ class Graphiques(QWidget):
       
     def initUI(self):
         
-        mainLayout = QHBoxLayout()
-        # variable pour le data du fichier
-        self.data =[]
-        #BOuton test
-        self.test = QPushButton("test")
-        self.test.clicked.connect(self.test_func)
+        self.layout = QVBoxLayout()
         
-        # Formulaire
-        self.formGroupBox = QGroupBox("Paramètres")
-        layout = QFormLayout()
+        # Initialize tab screen
+        self.tabs = QTabWidget()
+        self.tab1 = QWidget()	
+        self.tab2 = QWidget()
+
+        # Add tabs
+        self.tabs.addTab(self.tab1,"Tab 1")
+        self.tabs.addTab(self.tab2,"Tab 2")
+
         
-        self.choose_file = QtWidgets.QPushButton("Choisir le fichier csv")
-        self.choose_file.clicked.connect(self.ouvre_arborescence)
-        self.path = ''
+        self.tab1.layout = QHBoxLayout()
+        self.tab2.layout = QVBoxLayout()
         
-        
-        self.colx = QComboBox()
-        self.coly = QComboBox()
-        
-        self.tracer = QtWidgets.QPushButton("Tracer")
-        self.tracer.clicked.connect(self.Trace)
-        
-        # Choux couleur
-        self.color_btn = QtWidgets.QPushButton("Choisir couleur")
-        self.color_btn.clicked.connect(self.fig_color)
-        self.color = 'b'
-        
-        # Choix marqueur
-        marqueurs = Line2D.markers.keys()
-        self.marqueur = '.'
-        self.mark = QComboBox()
-        for i in marqueurs:
-            self.mark.addItem(str(i))
-            
-        self.mark.currentIndexChanged.connect(self.fig_marqueur)
-        
-        # linestyles
-        self.current_linestyle = '-'
-        linestyles = ['-', '--', '-.', ':','steps','None']
-        self.linestyle = QComboBox()
-        for i in linestyles:
-            self.linestyle.addItem(str(i))
-        self.linestyle.currentIndexChanged.connect(self.fig_linestyle)
-        
-        # tracer
-        self.fig = Figure()
-        self.sub1 = self.fig.add_subplot(111)
-        self.sub1.plot([],[],'r')
+# ------------------- Tab 1 (Graphique) -----------------------------        
+        self.TAB1 = Tab1(self)
+# -------------------- Tab 2 (données) ------------------------------
+        self.TAB2 = Tab2(self)
+# -------------------------------------------------------------------   
+    
+        self.tab1.setLayout(self.tab1.layout)
+        self.tab2.setLayout(self.tab2.layout)
         
         
-        
-        
-        self.canvas = FigureCanvas(self.fig)
-        mainLayout.addWidget(self.canvas)
-        
-        layout.addRow(self.test)
-        layout.addRow(self.choose_file)
-        layout.addRow(QtWidgets.QLabel("x: "), self.colx)
-        layout.addRow(QtWidgets.QLabel("y: "), self.coly)
-        layout.addRow(self.tracer)
-        layout.addRow(self.color_btn)
-        layout.addRow(QtWidgets.QLabel('Marqueur:'),self.mark)
-        layout.addRow(QtWidgets.QLabel('line:'),self.linestyle)
-        self.formGroupBox.setLayout(layout)
-        mainLayout.addWidget(self.formGroupBox)
-        
-        
-        self.setLayout(mainLayout)
+        self.layout.addWidget(self.tabs)
+        self.setLayout(self.layout)
         
 
         
@@ -153,13 +197,30 @@ class Graphiques(QWidget):
         
     # Ouvre fenetre de dialogue pour choisir le fichier (class Dialog)
     def ouvre_arborescence(self):
-        Dialog = dialog(self)
+        Dialog = dialog_fichier(self)
         Dialog.show()
+        
+        
         
     # recupere les données et remplis les listes déroulantes (x et y)
     def parse_file(self):
         self.data = pd.read_csv(self.path, delimiter=",")
         headers = self.data.columns.values.tolist()
+        
+        nl = self.data.shape[0]
+        ncol = self.data.shape[1]
+        self.TAB2.tableau.setColumnCount(ncol)
+        self.TAB2.tableau.setRowCount(nl)
+        self.TAB2.tableau.setHorizontalHeaderLabels(headers)
+        for i in range(nl):
+            for j in range(ncol):
+                self.TAB2.tableau.setItem(i,j,QTableWidgetItem(str(self.data.iloc[i][j])))
+
+        
+        print(self.data)
+        
+        
+        print(headers)
         for i in headers:
             self.colx.addItem(i)
             self.coly.addItem(i)
@@ -172,8 +233,7 @@ class Graphiques(QWidget):
             self.data_x = self.data[str(self.colx.currentText())]
             self.data_y = self.data[str(self.coly.currentText())]
             
-            self.sub1.plot(self.data_x, self.data_y, linestyle=self.current_linestyle, marker = self.marqueur, color=self.color)
-            self.canvas.draw()
+            self.replot_fig()
       
     # Efface et Retrace le figure 
     def replot_fig(self):
